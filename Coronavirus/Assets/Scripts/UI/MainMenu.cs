5360aff68add4b99047ed947b4546de7f602f7bd
miles <milesrhodes123@gmail.com>
2020-08-10 01:14:13 -0400

    Menu workings

---
+++ Coronavirus/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+              Application.Quit();
+        #endif
+    }
+}
