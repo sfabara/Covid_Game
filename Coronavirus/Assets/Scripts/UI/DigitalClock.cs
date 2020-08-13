1c4cf233edcab4720c8cd66fd8bd8b4872aa226c
Gastonb26 <GMBlanco@student.fullsail.edu>
2020-08-13 01:05:56 -0400

    Time System added

--- Coronavirus/Assets/Scripts/UI/DigitalClock.cs
+++ Coronavirus/Assets/Scripts/UI/DigitalClock.cs
@@ -7,22 +7,67 @@ using UnityEngine.UI;
 public class DigitalClock : MonoBehaviour
 {
 
-    public Text timerText;
-    public Text dayText; 
+    public bool DebugTimeLog = false;
 
-    public float minute, hour, day, month; 
-    
+    public Text hourText, dayText, monthText, yearText; 
+    public float hour, day, month, year; 
+
+    float elapsedTime;
+    public float Seconds_per_ingame_hour = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        hour = 0;
+        day = 0;
+        month = 1;
+        year = 2006; 
+
+        elapsedTime = 0; 
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        hourText.text = "Hour: " + hour.ToString();
+        dayText.text = "Day: " + day.ToString();
+        monthText.text = "Month: " + month.ToString();
+        yearText.text = "Year: " + year.ToString(); 
 
-        timerText.text = Time.time.ToString(); 
+        elapsedTime += Time.deltaTime;   
+
+       
+        if(elapsedTime >= Seconds_per_ingame_hour)
+        {
+            hour++;
+            elapsedTime = 0; 
+            
+            
+            if(hour == 24)
+            {
+                hour = 0;
+                day++; 
+            }
+            if(day == 30 )
+            {
+                month++;
+                day = 0; 
+            }
+            if(month == 12)
+            {
+                month = 0;
+                year++; 
+            }
+
+        }
+        
+
+        
+        Debug.Log("Hour: " + hour + " Day: " + day + " Month: " + month + " Year: " + year); 
+        Debug.Log(elapsedTime); 
+
     }
 }
