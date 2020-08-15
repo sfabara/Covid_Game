98189d9cb380b0523b82f35e38eea070811ddbcd
Ignacio <64559541+IggyBlanco@users.noreply.github.com>
2020-08-15 13:45:10 -0400

    Commit1

---
+++ Coronavirus/Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+
+    public GameObject currentInteractableObject = null;
+    public Interactable currentInteractableScript = null;
+    public Inventory inventory;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact") && currentInteractableObject)
+        {
+            // Check to see if obj is to be stored in inventory
+            if (currentInteractableScript.inventory)
+            {
+                inventory.AddItem(currentInteractableObject);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Interactable"))
+        {
+            Debug.Log(other.name);
+            currentInteractableObject = other.gameObject;
+            currentInteractableScript = currentInteractableObject.GetComponent<Interactable>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Interactable"))
+        {
+            if (other.gameObject == currentInteractableObject)
+            {
+                currentInteractableObject = null;
+            }
+        }
+    }
+
+}
---
+++ Coronavirus/Assets/Scripts/ToggleHolos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleHolos : MonoBehaviour
+{
+
+    void Update()
+    {
+        // Toggle Inventory
+        if (Input.GetButtonDown("Toggle Inventory"))
+        {
+            if (gameObject.transform.GetChild(0).gameObject.activeSelf == true)
+                gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            else
+                gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        }
+
+    }
+}
