332a693452e4f9c3d28a5d9756cfc3a3b8b58681
Gastonb26 <GMBlanco@student.fullsail.edu>
2020-08-19 14:18:02 -0400

    NPC scripts

---
+++ Coronavirus/Assets/Scripts/NPC.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+
+    public string npcName; //for text 
+    public Image npcImage;
+
+    public float endurance, speed, health; 
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Coronavirus/Assets/Scripts/UI/NPCPanel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class NPCPanel : MonoBehaviour
+{
+
+    public Image npcImage; 
+    public TextMeshProUGUI npcName;
+    public TextMeshProUGUI npcHealthText; 
+    public TextMeshProUGUI npcEnduranceText; 
+    public TextMeshProUGUI npcSpeedText; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
